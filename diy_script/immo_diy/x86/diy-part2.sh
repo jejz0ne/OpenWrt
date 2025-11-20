591d937527cc99bed66d4e96aec2179f8c789b07
jejz0ne <jejz.1688@gmail.com>
2025-11-21 07:29:18 +0800

    更新 diy-part2.sh

--- diy_script/immo_diy/x86/diy-part2.sh
+++ diy_script/immo_diy/x86/diy-part2.sh
@@ -74,6 +74,12 @@ sed -i '$a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sy
 sed -i '$a net.core.wmem_max=16777216' package/base-files/files/etc/sysctl.conf
 sed -i '$a net.core.rmem_max=16777216' package/base-files/files/etc/sysctl.conf
 
+# 删除所有包含 luci-app-attendedsysupgrade 的 Makefile 行
+for f in $(grep -rl 'luci-app-attendedsysupgrade' package feeds | grep 'Makefile$'); do
+    echo "Cleaning $f ..."
+    sed -i '/luci-app-attendedsysupgrade/d' "$f"
+done
+
 # 报错修复
 # sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile
 if [ "$REPO_BRANCH" != "openwrt-23.05" ]; then
