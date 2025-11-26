82861da2bfcd6230340b8b3cddad17b222c55d1b
jejz0ne <jejz.1688@gmail.com>
2025-11-26 10:31:14 +0800

    更新 diy-part2.sh

--- diy_script/immo_diy/x86/diy-part2.sh
+++ diy_script/immo_diy/x86/diy-part2.sh
@@ -91,6 +91,9 @@ if [ "$REPO_BRANCH" != "openwrt-23.05" ]; then
     echo "修复完成……"
 fi
 
+# 修复ramfree位置问题
+sed -i '/"order":/{s/\([0-9]\+\)/"\1"/}' feeds/luci/applications/luci-app-ramfree/root/usr/share/luci/menu.d/luci-app-ramfree.json
+
 # 添加整个源仓库(git_clone)/添加源仓库内的指定目录(clone_dir)/添加源仓库内的所有目录(clone_all)
 # filebrowser luci-app-pushbot
 clone_dir main https://github.com/xiangfeidexiaohuo/2305-ipk luci-app-adguardhome luci-app-pushbot luci-app-poweroff
